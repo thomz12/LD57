208d55d2fadbe86d323920a02834362e1c6290d6
thomz12 <thomz12@hotmail.nl>
2025-04-06 11:02:45 +0200

    added background

--- resources/scripts/timer.lua
+++ resources/scripts/timer.lua
@@ -1,7 +1,17 @@
 time = 0.0
 
+local player = nil
+
+function start()
+    player = find_entity("submarine")
+end
+
 function update(delta)
-    time = time + delta
+    if player.scripts.submarine.game_over then
+        time = time + delta * 500
+    else
+        time = time + delta
+    end
     local minutes = math.floor(time / 60)
     local seconds = math.floor(time % 60)
     entity.ui_text.text = string.format("%02.f:%02.f", minutes, seconds)
